079077a9fe1c78070c38e1804103fccdf51f99d2
Ghost <1129.gh0st@gmail.com>
2023-01-06 00:12:09 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-import './App.css';
+
 import Users from './components/Users';
 
 function App() {
   return (
     <div className="App">
       <Users/>
-
-     
-        {/* <Counter /> */}
-       
     </div>
   );
 }
